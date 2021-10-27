9e76373fb17f38659d1cd5f8dbb17ff92a5bf818
Filipe Augusto Santos de Moura <85424389+Filipey@users.noreply.github.com>
2021-10-27 14:21:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,13 +21,15 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-})
+function changeHeaderWhenScroll() {
+  window.addEventListener('scroll', function () {
+    if (window.scrollY >= navHeight) {
+      header.classList.add('scroll')
+    } else {
+      header.classList.remove('scroll')
+    }
+  })
+}
 
 /* TESTIMONIALS CAROUSEL SLIDER SWIPER */
 const swiper = new Swiper('.swiper-container', {
@@ -36,7 +38,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* SCROLLREVEAL: MOSTRAR ELEMENTOS QUANDO DER SCROLL NA PAGINAA */
@@ -52,7 +60,48 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials testimonials,
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer.social
 `,
   { interval: 100 }
 )
+
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* MENU ATIVO CONFORME SESSÃO NA PÁGINA */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document.querySelector('nav ul li a[href*=]' + sectionId + ']')
+      classList.add('active')
+    } else {
+      document.querySelector('nav ul li a[href*=]' + sectionId + ']')
+      classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
